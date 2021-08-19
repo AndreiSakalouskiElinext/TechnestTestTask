a00276c443d4b9c1ab7ae348b5205238768ad806
Andrei Sakalouski <AndrSakalouski@elinext.com>
2021-08-19 17:09:07 +0300

    Added indecs and commands ts files. Added a fixture with credentials Added a spec prototype

---
+++ cypress/integration/articles.spec.ts
@@ -0,0 +1,10 @@
+describe('Creating articles feature', () => {
+    beforeEach('Log in to the application', () => {
+    })
+
+    it('Creates articles and verifies their number', () => {
+    })
+
+    after('Deletes all the articles', () => {
+    })
+})
